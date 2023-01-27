d406735aa4e6306a90404d0a11eb395029e3778e
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-01-27 20:43:24 +0300

    fixed

--- car/models.py
+++ car/models.py
@@ -25,5 +25,12 @@ class Reservation(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='cars')
     start_date = models.DateTimeField()
     end_date= models.DateTimeField()
+    
     def __str__(self):
         return f' Customer {self.customer} reserved {self.car}'
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields = ["customer", "start_date", "end_date"],  name="user_rent_date"
+            )
+        ]

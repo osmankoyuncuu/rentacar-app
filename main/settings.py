9b5f9202b566fa0c88e19866776d0fc665094ea6
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-01-27 14:20:19 +0300

    user finish

--- main/settings.py
+++ main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     #third party
     'rest_framework',
     'dj_rest_auth',
+    'rest_framework.authtoken',
     #myapp
     'users',
 ]
@@ -126,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
